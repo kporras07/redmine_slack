559997eab687f2b3eeba30931ffda091559cdbd9
Kevin Porras <kporras07@gmail.com>
2020-06-23 08:26:38 -0600

    Initial tests support.

---
+++ test/integration/routing_test.rb
@@ -0,0 +1,8 @@
+require File.expand_path('../../test_helper', __FILE__)
+
+class RoutingTest < Redmine::RoutingTest
+  test 'routing redmine_slack' do
+    should_route 'GET /projects/1/settings/redmine_slack' => 'projects#settings', id: '1', tab: 'redmine_slack'
+    should_route 'PUT /projects/1/redmine_slack_setting' => 'redmine_slack_settings#update', project_id: '1'
+  end
+end
---
+++ test/unit/i18n_test.rb
@@ -0,0 +1,36 @@
+require File.expand_path('../../test_helper', __FILE__)
+
+class I18nTest < ActiveSupport::TestCase
+  include Redmine::I18n
+
+  def setup
+    User.current = nil
+  end
+
+  def teardown
+    set_language_if_valid 'en'
+  end
+
+  def test_valid_languages
+    assert valid_languages.is_a?(Array)
+    assert valid_languages.first.is_a?(Symbol)
+  end
+
+  def test_locales_validness
+    lang_files_count = Dir[Rails.root.join('plugins',
+                                           'redmine_slack',
+                                           'config',
+                                           'locales',
+                                           '*.yml')].size
+    assert_equal lang_files_count, 4
+    valid_languages.each do |lang|
+      assert set_language_if_valid(lang)
+    end
+    # check if parse error exists
+    ::I18n.locale = 'fr'
+    assert_equal 'Redmine Slack Settings', l(:label_redmine_slack_setting)
+    ::I18n.locale = 'en'
+    assert_equal 'Redmine Slack Settings', l(:label_redmine_slack_setting)
+    set_language_if_valid('en')
+  end
+end
