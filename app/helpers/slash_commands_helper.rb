c1074a354a6c6828248db51d3d831c2fe3ade243
Kevin Porras <kporras07@gmail.com>
2020-06-29 14:00:31 -0600

    Fix coding standards.

--- app/helpers/slash_commands_helper.rb
+++ app/helpers/slash_commands_helper.rb
@@ -8,6 +8,6 @@ module SlashCommandsHelper
     string_to_validate = "v0:#{timestamp}:#{body}"
     digest = OpenSSL::Digest.new('sha256')
     signed = OpenSSL::HMAC.hexdigest(digest, signing_secret, string_to_validate)
-    "v0=#{signed}" == signature
+    signature == "v0=#{signed}"
   end
 end
